12c35d0f551c7c6641247e0639ad027a1051566f
Abhishek Singh <singh2811abhishek@gmail.com>
2024-05-17 22:21:00 +0530

    update

--- index.js
+++ index.js
@@ -17,15 +17,27 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     
-    if (token == null) return res.sendStatus(401); // No token provided
+    if (token == null) return res.sendStatus(401); 
   
     jwt.verify(token, 'your_jwt_secret_key', (err, user) => {
-      if (err) return res.sendStatus(403); // Token is not valid
+      if (err) return res.sendStatus(403); 
       req.user = user;
       next();
     });
   };
-
+  
+  app.get('/profile', authenticateToken, async (req, res) => {
+      try {
+        const user = await StepUserModel.findById(req.email);
+        if (!user) {
+          return res.status(404).send('User not found');
+        }
+        res.json(user);
+      } catch (error) {
+        console.error('Error fetching profile:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
 
 app.post("/stepregister", async (req, res) => {
     try {
@@ -74,18 +86,6 @@ app.post("/stepuserlogin", async (req, res) => {
     }
 });
 
-app.get('/profile', authenticateToken, async (req, res) => {
-    try {
-      const user = await User.findById(req.user._id);
-      if (!user) {
-        return res.status(404).send('User not found');
-      }
-      res.json(user);
-    } catch (error) {
-      console.error('Error fetching profile:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
 
 
 app.post("/ngoregister", async (req, res) => {
